b6b8eb0adbadd077d7f2f8fa231b861fa4c63834
Unknown <unknown@example.com>
2020-07-29 07:54:23 -0300

    criei json tarefa
    minha aula json

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -5,5 +5,10 @@
 // 3 - Crie uma function para acrescentar uma nova tarefa nessa lista
 
 // 4 - Crie uma function para alterar o status da tarefa na lista
-var = familia {"familia":"alves",
+var familia = {"familia":"alves",
 "integrantes":"fhelipe"}
+
+var tarefa = {
+    "nome": "estudar",
+    "status": false
+}
