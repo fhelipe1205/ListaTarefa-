639193698e69a37208b4a7b94a60e9feeb7f2805
Fhelipe <fhelipehernani@gmail.com>
2020-07-29 08:03:02 -0300

    teste perfil
    tentando  mudar nome perfil
    no git kraken

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -3,12 +3,13 @@
 // 2 - Cria uma lista de 5 tarefas
 
 // 3 - Crie uma function para acrescentar uma nova tarefa nessa lista
-
-// 4 - Crie uma function para alterar o status da tarefa na lista
 var familia = {"familia":"alves",
 "integrantes":"fhelipe"}
 
 var tarefa = {
     "nome": "estudar",
     "status": false
+}
+function novaTarefa(nome){
+
 }
