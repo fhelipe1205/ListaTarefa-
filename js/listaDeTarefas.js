5af1c17c73e8afb642d312bc0efdbf05bec97c84
Unknown <unknown@example.com>
2020-07-29 07:33:02 -0300

    Meu primeiro commit
    aula de json

---
+++ js/listaDeTarefas.js
@@ -0,0 +1,9 @@
+// 1 - Criar objeto de apenas uma tarefa. 
+
+// 2 - Cria uma lista de 5 tarefas
+
+// 3 - Crie uma function para acrescentar uma nova tarefa nessa lista
+
+// 4 - Crie uma function para alterar o status da tarefa na lista
+var = familia {"familia":"alves",
+"integrantes":"fhelipe"}
